ad00428bb5bbb9d26acb133721d9bd5bf08971e2
voicenoid <sobarakugan@gmail.com>
2023-10-22 00:58:15 +0900

    URLの修正5

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       <ThemeProvider theme ={theme}>
       <AppBar position="static" >
         <Toolbar>
-          <Typography ><Link href ="/" sx={{color:"white"}}>謎解きアプリ</Link></Typography>
+          <Typography ><Link href ="/kfes-gk" sx={{color:"white"}}>謎解きアプリ</Link></Typography>
         </Toolbar>
       </AppBar>
       <Container fixed>
