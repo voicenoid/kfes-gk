f9290d10218d4c482ab508492662c9b9e274b0a8
voicenoid <sobarakugan@gmail.com>
2023-12-04 14:48:22 +0900

    動画再生ページの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Home from './routes/homePage';
 import { Route, Routes } from 'react-router';
 import ErrorPage from './routes/errorPage';
 import Questions from './routes/questions';
-import {AppBar,Box,Toolbar,Typography,Container} from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Container, IconButton, Link } from '@mui/material';
 import '@fontsource/roboto/500.css';
-import { createTheme,ThemeProvider } from '@mui/material/styles';
-import { Link } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import VideoPage from "./routes/videoPage";
+import { Movie } from '@mui/icons-material';
 
 
 function App() {
@@ -23,22 +23,29 @@ function App() {
 
   return (
     <div>
-      <ThemeProvider theme ={theme}>
-      <AppBar position="static" >
-        <Toolbar>
-          <Typography ><Link href ="/kfes-gk" sx={{color:"white"}}>緊急クエスト</Link></Typography>
-        </Toolbar>
-      </AppBar>
-      <Container fixed>
-        <Box sx={{ margin: 0.5, marginTop: 2,bgcolor: 'background.paper',borderRadius: 2}}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/questions/:id" element={<Questions />} />
-            <Route path="*" element={<ErrorPage />} />
-            <Route path="/video" element={<VideoPage />} />
-          </Routes>
-        </Box>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link href="/" sx={{ color: "white" }} underline='none'>緊急クエスト</Link>
+            </Typography>
+            <IconButton color="inherit">
+              <Link href="/video" sx={{ color: "white" }} underline='none'>
+                <Movie />
+              </Link>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Container fixed>
+          <Box sx={{ margin: 0.5, marginTop: 2, bgcolor: 'background.paper', borderRadius: 2 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/questions/:id" element={<Questions />} />
+              <Route path="/video" element={<VideoPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </Box>
+        </Container>
       </ThemeProvider>
     </div>
   );
